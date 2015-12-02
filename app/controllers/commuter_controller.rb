a03c475d51596fa9bf0abfc54543ec683367f444
Christopher M. Wood <cmw2379@vt.edu>
2015-12-02 11:53:11 -0500

    navigation update

--- app/controllers/commuter_controller.rb
+++ app/controllers/commuter_controller.rb
@@ -1,10 +1,10 @@
 class CommuterController < ApplicationController
 
-  before_filter :validateAuthToken, :verify_commuter
+  before_filter :verify_commuter
 
   def dashboard
-    user = get_active_user
-    @reviews = user.reviews
+    @user = get_active_user
+    @reviews = @user.reviews
   end
 
   def truck_profile
