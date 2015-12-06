fc5fb63c599ccb274f8fd20261c322392c3111e3
Christopher M. Wood <cmw2379@vt.edu>
2015-12-06 10:33:40 -0500

    update to commuter pages

--- app/assets/javascripts/commuter/reviews.js
+++ app/assets/javascripts/commuter/reviews.js
@@ -1,3 +1,27 @@
 $(function() {
     $("#reviews-table").DataTable();
 });
+
+function viewReviewPopup(review_id) {
+    $(".view_review_popup_input").val("");
+
+    var parameters = {};
+    parameters['version'] = 'v1';
+    parameters['api_name'] = 'company';
+    parameters['api_method'] = 'get-review';
+    parameters['review_id'] = review_id;
+
+    api_request(parameters, function(response){
+        if(response['success']) {
+            $("#popup_reviewer_name").text(response['reviewer_name']);
+            $("#popup_reviewer_email").text(response['reviewer_email']);
+            $("#popup_review_type").text(response['review_type']);
+            $("#popup_review_tree").text(response['review_tree']);
+            $("#popup_review_text").val(response['review_comment']);
+            $("#view_review_popup").bPopup();
+        }
+        else {
+            alert("Error: Problem opening review. Please try again soon.");
+        }
+    });
+}
